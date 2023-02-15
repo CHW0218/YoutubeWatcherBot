bbc9f089c5b1bf17cb07d305fb500d4e2379b487
CHW0218 <caihaowen02118@gmail.com>
2023-02-15 13:16:05 -0500

    uploading source code

---
+++ config.py
@@ -0,0 +1,4 @@
+config = {
+    "google_api_key":"AIzaSyAHS0ws_hds_HgQea4UDZPSjLU1xnZW8oA",
+    "youtube_playlist_id": "PLqc7hbQ9QIkM-zeXAKEjFKMa7SO4vsd6a",
+}
---
+++ youtube_watcher.py
@@ -0,0 +1,66 @@
+import logging
+import sys
+import requests
+from config import config
+import json
+from pprint import pformat
+
+def fetch_playlist_items_page(google_api_key,youtube_playlist_id, page_token=None):
+    response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems", params={
+        "key": google_api_key,
+        "playlistId": youtube_playlist_id,
+        "part": "contentDetails",
+        "pageToken": page_token,
+        })
+    payload = json.loads(response.text)
+    logging.debug("GOT: %s", payload)
+    return payload
+
+def fetch_playlist_items(google_api_key,youtube_playlist_id, page_token=None):
+    payload = fetch_playlist_items_page(google_api_key,youtube_playlist_id,page_token)
+    yield from payload['items']
+    next_page_token = payload.get('nextPageToken')
+    if next_page_token is not None:
+        yield from fetch_playlist_items(google_api_key,youtube_playlist_id, next_page_token)
+
+def fetch_video_page(google_api_key,video_id, page_token=None):
+    response = requests.get("https://www.googleapis.com/youtube/v3/videos", params={
+        "key": google_api_key,
+        "id": video_id,
+        "part": "snippet,statistics",
+        "pageToken": page_token,
+        })
+    payload = json.loads(response.text)
+    logging.debug("GOT: %s", payload)
+    return payload
+
+
+def fetch_video(google_api_key,youtube_playlist_id, page_token=None):
+    payload = fetch_video_page(google_api_key,youtube_playlist_id,page_token)
+    yield from payload['items']
+    next_page_token = payload.get('nextPageToken')
+    if next_page_token is not None:
+        yield from fetch_video(google_api_key,youtube_playlist_id, next_page_token)
+
+def summarize_video(video):
+    return{
+        "video_id": video['id'],
+        "title": video['snippet']['title'],
+        "views": int(video['statistics'].get('viewCount',0)),
+        "likes": int(video['statistics'].get('likeCount',0)),
+        "comments": int(video['statistics'].get('commentCount',0)),
+    }
+
+def main():
+    logging.info('Starting youtube_watcher')
+    google_api_key = config['google_api_key']
+    youtube_playlist_id = config['youtube_playlist_id']
+    for video_item in fetch_playlist_items(google_api_key,youtube_playlist_id):
+        video_id = video_item['contentDetails']['videoId']
+        for video in fetch_video(google_api_key,video_id):
+            logging.info("Video: %s", pformat(summarize_video(video)))
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    sys.exit(main())
