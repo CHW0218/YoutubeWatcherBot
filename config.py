73fb2d1038b71efeda2c4b5e8ca19b0e30a40fe9
CHW0218 <caihaowen02118@gmail.com>
2023-02-15 13:21:32 -0500

    update

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
 config = {
-    "google_api_key":"AIzaSyAHS0ws_hds_HgQea4UDZPSjLU1xnZW8oA",
+    "google_api_key": "<your key>",
     "youtube_playlist_id": "PLqc7hbQ9QIkM-zeXAKEjFKMa7SO4vsd6a",
+    "kafka": {
+        "bootstrap.servers": "<your serve>",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "<your key>",
+        "sasl.password": "<your secret>",
+    },
+    "schema_registry": {
+        "url": "<your SR server>",
+        "basic.auth.user.info": "<your SR key>:<your SR secret>",
+    }
 }
