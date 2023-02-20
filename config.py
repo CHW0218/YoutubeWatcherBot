b1865991930c553f136b7df1cea49d1d048442d2
CHW0218 <caihaowen02118@gmail.com>
2023-02-20 08:41:50 -0500

    Revert "update"
    This reverts commit 73fb2d1038b71efeda2c4b5e8ca19b0e30a40fe9.

--- config.py
+++ config.py
@@ -1,15 +1,4 @@
 config = {
-    "google_api_key": "<your key>",
+    "google_api_key":"AIzaSyAHS0ws_hds_HgQea4UDZPSjLU1xnZW8oA",
     "youtube_playlist_id": "PLqc7hbQ9QIkM-zeXAKEjFKMa7SO4vsd6a",
-    "kafka": {
-        "bootstrap.servers": "<your serve>",
-        "security.protocol": "sasl_ssl",
-        "sasl.mechanism": "PLAIN",
-        "sasl.username": "<your key>",
-        "sasl.password": "<your secret>",
-    },
-    "schema_registry": {
-        "url": "<your SR server>",
-        "basic.auth.user.info": "<your SR key>:<your SR secret>",
-    }
 }
