89c3b88ac8558bf5e74d079f74798bcc35998e20
CHW0218 <caihaowen02118@gmail.com>
2023-02-20 16:47:46 -0500

    Update bot& delete library file

--- config.py
+++
@@ -1,4 +0,0 @@
-config = {
-    "google_api_key":"AIzaSyAHS0ws_hds_HgQea4UDZPSjLU1xnZW8oA",
-    "youtube_playlist_id": "PLqc7hbQ9QIkM-zeXAKEjFKMa7SO4vsd6a",
-}
---
+++ configsample.py
@@ -0,0 +1,16 @@
+config = {
+    "google_api_key": "<your key>",
+    "youtube_playlist_id": "PLqc7hbQ9QIkM-zeXAKEjFKMa7SO4vsd6a",
+    "kafka": {
+        "bootstrap.servers": "<your serve>",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "<your key>",
+        "sasl.password": "<your secret>",
+    },
+    "schema_registry": {
+        "url": "<your SR server>",
+        "basic.auth.user.info": "<your SR key>:<your SR secret>",
+    }
+}
+
